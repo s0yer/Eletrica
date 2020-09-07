d4bd7e4842f3c60b2de4ace48231b32dba127325
jadsonmarliere <jds.mdo@gmail.com>
2020-09-07 10:01:57 -0300

    Update eletromagnetismo.py

--- eletromagnetismo.py
+++ eletromagnetismo.py
@@ -4,10 +4,17 @@
 
 def valores_fonte_test():
 
+def calcula_modulo(posicoes):
+    modulo_posicoes = pow((pow(posicoes[0], 2) + pow(posicoes[1], 2) + pow(posicoes[2], 2)), 1/2)
+
+    return modulo_posicoes
+
 def recebe_valores_usuario():
     qtd_cargas  = input('Digite a quantidade de cargas: ')
     lista_cargas = []
     lista_cartesiana = []
+    lista_r = []
+
     n = 1
     print(' Digite primeiramente a carga de prova :')
 
@@ -17,6 +24,7 @@ def recebe_valores_usuario():
 
         # recebe coordenadas do espaco:
         lista_posicoes = []
+        print('Se alguma posicoes')
         lista_posicoes.append('digite a posicao no eixo X: ')
         lista_posicoes.append('digite a posicao no eixo Y: ')
         lista_posicoes.append('digite a posicao no eixo Z: ')
@@ -25,15 +33,24 @@ def recebe_valores_usuario():
         lista_cartesiana.append(lista_posicoes)
 
         n =+ 1
+    for in range(qtd_cargas):
+        lis
 
-    return lista_cargas
+    return lista_cargas, lista_posicoes
 
 def calcula_forca_resultante():
-    lista_cargas = recebe_valores_usuario()
+    lista_cargas, lista_posicoes = recebe_valores_usuario()
+
+    lista_resposta = []
     # constante de permissividade
     e_const = 8.854 * pow(10, -12)
     k = 1 / (4 * 3.1415 * e_const)
 
+    r12 =
+
+    ans = 'Forca Resultante: {.2e} + {.2e} + {.2e} (N)'lista_resposta[]
 def calcula_campo_eletrico():
+
+
 
 def main_screen():
