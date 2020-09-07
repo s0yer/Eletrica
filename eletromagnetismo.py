df22b2d6dee2b5c322a2be87da2a38249ebe52a8
jadsonmarliere <jds.mdo@gmail.com>
2020-09-07 09:27:54 -0300

    Update eletromagnetismo.py

--- eletromagnetismo.py
+++ eletromagnetismo.py
@@ -5,14 +5,25 @@
 def valores_fonte_test():
 
 def recebe_valores_usuario():
-    qtd_cargas  = input('Digite a quantidade de cargas')
+    qtd_cargas  = input('Digite a quantidade de cargas: ')
     lista_cargas = []
     lista_cartesiana = []
     n = 1
     print(' Digite primeiramente a carga de prova :')
+
     for elem in range(qtd_cargas):
+        # recebe valor da carga
         lista_cargas.append(input('Digite a {} carga'.format(n)))
-        lista_cartesiana.append('digite a posicao espacial da carga no formato: xyz (sem espaco) ex: para p1(5,-3,4) ->5-35')
+
+        # recebe coordenadas do espaco:
+        lista_posicoes = []
+        lista_posicoes.append('digite a posicao no eixo X: ')
+        lista_posicoes.append('digite a posicao no eixo Y: ')
+        lista_posicoes.append('digite a posicao no eixo Z: ')
+
+        # adicona coodenadas no array de posicoes
+        lista_cartesiana.append(lista_posicoes)
+
         n =+ 1
 
     return lista_cargas
